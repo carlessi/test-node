e5547ff19628ccab44896b2448a78268f5eb696d
carlessi <74634054+carlessi@users.noreply.github.com>
2021-12-29 23:39:59 -0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -16,36 +16,6 @@ const server = http.createServer(function(req, res) {
    res.end('Hello World\n');
 });
 
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-});
-
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}!`)
-});
-
-const express = require('express');
-const app = express();
-
-exports.area = function(width) { return width * width; };
-exports.perimeter = function(width) { return 4 * width; };
-const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
-console.log('The area of a square with a width of 4 is ' + square.area(4));
-module.exports = {
-  area: function(width) {
-    return width * width;
-  },
-
-  perimeter: function(width) {
-    return 4 * width;
-  }
-};
-
-
 
 // Prints a log once the server starts listening
 server.listen(port, hostname, function() {
